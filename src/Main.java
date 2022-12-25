c8da6eea9d454333ce51edc02cdcbd6fd307c9ea
ViktorK <nokksaxs@gmail.com>
2022-12-25 23:45:05 +0300

    added task4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+//Given an input string s, reverse the order of the words.
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        int start = s.length() - 1;
+        int end = s.length();
+        while (start >= 0) {
+            while (start >= 0 && s.charAt(start) == ' ') {
+                start--;
+            }
+            end = start + 1;
+            while (start >= 0 && s.charAt(start) != ' ') {
+                start--;
+            }
+            result.append(s, start + 1, end).append(' ');
+        }
+        if (result.length() > 0) {
+            result.setLength(result.length() - 1);
+        }
+        return result.toString();
     }
 }
